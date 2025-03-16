2090a377cd1edc825f239270f338ff2d66f09f82
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2025-03-16 14:37:44 +0000

    react-auth project :auth

--- react-auth/backend/app.js
+++ react-auth/backend/app.js
@@ -1,27 +1,31 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 
-const eventRoutes = require('./routes/events');
-const authRoutes = require('./routes/auth');
+const eventRoutes = require("./routes/events");
+const authRoutes = require("./routes/auth");
 
+const PORT = 8080;
+const HOST = "localhost";
 const app = express();
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
 
 app.use(authRoutes);
 
-app.use('/events', eventRoutes);
+app.use("/events", eventRoutes);
 
 app.use((error, req, res, next) => {
   const status = error.status || 500;
-  const message = error.message || 'Something went wrong.';
+  const message = error.message || "Something went wrong.";
   res.status(status).json({ message: message });
 });
 
-app.listen(8080);
+app.listen((PORT, HOST), () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
+});
