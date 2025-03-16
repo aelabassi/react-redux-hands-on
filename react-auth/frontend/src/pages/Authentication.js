8ed1f0b56168477392460b4e76bf45ec4c20ce2c
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2025-03-16 14:37:33 +0000

    react-auth project :auth

--- react-auth/frontend/src/pages/Authentication.js
+++ react-auth/frontend/src/pages/Authentication.js
@@ -1,7 +1,33 @@
-import AuthForm from '../components/AuthForm';
+import AuthForm from "../components/AuthForm";
+import { redirect } from "react-router-dom";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get("mode") || "login";
+  if (mode !== "login" && mode !== "signup") {
+    throw new Response("Unsupported mode", { status: 422 });
+  }
+  const data = await request.formData();
+  const authData = {
+    email: data.get("email"),
+    password: data.get("password"),
+  };
+
+  const response = await fetch("http://localhost:8000/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+  if (!response.ok) {
+    throw new Response("Authentication failed", { status: 401 });
+  }
+  return redirect("/");
+}
